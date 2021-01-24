6d6f25ab991bb8791b989b7630da3fd7fe21b358
Kristian Dimitrov <kristiandimitrov12@gmail.com>
2021-01-24 10:33:57 +0200

    Adding JuicyPixels library and Export module

---
+++ src/Export.hs
@@ -0,0 +1,19 @@
+module Export where
+
+import Base
+import Shading.FrameBuffer
+import Shading.Color
+import Codec.Picture
+
+generatorFunc :: FrameBuffer Rgb -> Int -> Int -> PixelRGBF
+generatorFunc (FrameBuffer w _ buffer) x y = PixelRGBF rr gg bb
+  where index = y * w + x
+        (Rgb rr gg bb) = buffer !! index
+
+frameBufferToDynamicImage :: FrameBuffer Rgb -> DynamicImage
+frameBufferToDynamicImage fBuffer@(FrameBuffer w h _) =
+  ImageRGBF (generateImage (generatorFunc fBuffer) w h)  
+
+saveImageAsPng :: FilePath -> FrameBuffer Rgb -> IO ()
+saveImageAsPng path fBuffer =
+  savePngImage path $ frameBufferToDynamicImage fBuffer
