c7f8c59e5f9f20e2f8e068368f788c4b033661d3
mstefanov99 <mstefanov99@gmail.com>
2021-01-29 18:55:16 +0200

    Implementing checker sampler

--- src/Shading/Sampler.hs
+++ src/Shading/Sampler.hs
@@ -3,9 +3,20 @@ module Shading.Sampler where
 import Base
 import Shading.Color
 
-data Sampler = ConstantColorSampler {
-  color :: Rgb
-} deriving (Show, Eq)
+data Sampler = 
+  ConstantColorSampler {
+    color :: Rgb
+  } |
+  CheckerSampler {
+    size :: Float 
+  } deriving (Show, Eq)
 
 sample :: Sampler -> Texel -> Rgb
 sample (ConstantColorSampler color) _ = color
+sample (CheckerSampler size) (u, v) = let
+  x = mod (floor $ u / size :: Int) 2
+  y = mod (floor $ v / size :: Int) 2
+  in
+    if x == y 
+      then white
+      else Rgb 0 0 0
